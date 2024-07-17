5e33c486c025f3cb2bba34adb34fab47e462cc9f
mendesjar <ga0301@outlook.com>
2024-07-17 16:33:51 -0300

    feat: create function random hexCode color

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
-window.onload = function(){
-    const wrapper = document.getElementById("tiles");
+window.onload = function () {
+  const wrapper = document.getElementById("tiles");
 
-    let columns = 0,
+  let columns = 0,
     rows = 0;
 
-    const colors = [
-        "#192A8C",
-        "#1C92A7",
-        "#36D791",
-        "#E4DA4B",
-        "#7451DF",
-        "#E34C4A"
-    ]
+  const randomHexColorCode = () => {
+    let hexCode = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+    return "#" + hexCode;
+  };
 
-    let count = -1
+  let count = -1;
 
-    const handleOnClick = index => {
-        count = count + 1
+  const handleOnClick = (index) => {
+    count = count + 1;
 
-        anime({
-            targets: ".tile",
-            backgroundColor: colors[count % (colors.length - 1)],
-            delay: anime.stagger(50, {
-                grid: [columns, rows],
-                from: index
-            }),
-        })
-    }
+    anime({
+      targets: ".tile",
+      backgroundColor: randomHexColorCode(),
+      delay: anime.stagger(50, {
+        grid: [rows, columns],
+        from: index,
+      }),
+    });
+  };
 
-    const createTile = index => {
-        const tile = document.createElement("div");
+  const createTile = (index) => {
+    const tile = document.createElement("div");
+    tile.classList.add("tile");
+    tile.onclick = (e) => handleOnClick(index);
+    return tile;
+  };
 
-        tile.classList.add("tile");
+  const createTiles = (quatity) => {
+    Array.from(Array(quatity)).map((_, index) => {
+      wrapper.appendChild(createTile(index));
+    });
+  };
 
-        tile.onclick = e => handleOnClick(index);
+  const createGrid = () => {
+    wrapper.innerHTML = "";
+    columns = Math.floor(document.body.clientWidth / 50);
+    rows = Math.floor(document.body.clientHeight / 50);
 
-        return tile;
-    }
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
 
-    const createTiles = quatity => {
-        Array.from(Array(quatity)).map((tile, index) => {
-            wrapper.appendChild(createTile(index));
-        })
-    }
+    createTiles(columns * rows);
+  };
 
-    const createGrid = () => {
-        wrapper.innerHTML = "";
-        columns = Math.floor(document.body.clientWidth / 50);
-        rows = Math.floor(document.body.clientHeight / 50);
-
-        wrapper.style.setProperty("--columns", columns);
-        wrapper.style.setProperty("--rows", rows);
-
-        createTiles(columns * rows);
-    }
-
-    window.onresize = () => createGrid();
-}
+  window.onresize = () => createGrid();
+  createGrid();
+};
